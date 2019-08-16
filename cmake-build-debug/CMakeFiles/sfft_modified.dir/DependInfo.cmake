f338be7d9204f91e5c0645605cf5c68d19b1dba1
yagudinamir <yagudin.amir@mail.ru>
2019-08-16 18:38:05 +0200

    cmake build

---
+++ cmake-build-debug/CMakeFiles/sfft_modified.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/cachemisses.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/cachemisses.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/common.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/common.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/computefourier-1.0-2.0.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/computefourier-1.0-2.0.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/computefourier-3.0.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/computefourier-3.0.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/fftw.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/fftw.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/filters.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/filters.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/flopcount/flopcount.cpp" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/flopcount/flopcount.cpp.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/instruction_count.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/instruction_count.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/parameters.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/parameters.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/roofline.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/roofline.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/sfft.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/sfft.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/simulation.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/simulation.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/timer.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/timer.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/timing.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/timing.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/timing_many.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/timing_many.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/utils.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/utils.cc.o"
+  "/Users/amir/Desktop/sfft-hikp/sfft-modified/src/verification.cc" "/Users/amir/Desktop/sfft-hikp/sfft-modified/cmake-build-debug/CMakeFiles/sfft_modified.dir/src/verification.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/sfft_modified.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfft_modified.dir/src/flopcount/flopcount.cpp.o"
+  "CMakeFiles/sfft_modified.dir/src/cachemisses.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/common.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/computefourier-1.0-2.0.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/computefourier-3.0.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/fftw.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/filters.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/instruction_count.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/parameters.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/roofline.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/sfft.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/simulation.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/timer.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/timing.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/timing_many.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/utils.cc.o"
+  "CMakeFiles/sfft_modified.dir/src/verification.cc.o"
+  "sfft_modified.pdb"
+  "sfft_modified"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfft_modified.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
